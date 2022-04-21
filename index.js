04ccb192159ec811b8a2fa776ac060b701efd5ab
brian <erinbeech@icloud.com>
2022-04-21 18:31:31 -0400

    commit message

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+function appendCat(name) {
+    const copyofcats = [...cats,name]
+    return copyofcats
+}
+
+function  prependCat(name) {
+    const copyofcats = [name,...cats]
+    return copyofcats
+}
+function removeLastCat() {
+    const copyofcats = [...cats]
+    copyofcats.pop()
+    return copyofcats
+}
+
+function removeFirstCat() {
+    const copyofcats = [...cats]
+    copyofcats.shift()
+    return copyofcats
+}
